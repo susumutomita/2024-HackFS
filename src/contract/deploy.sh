166741fd1b82835a498eecd9a170a37d823ec995
susumu tomita <oyster880@gmail.com>
2024-06-01 08:19:37 +0900

    update deploy script

--- src/contract/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 環境変数の読み込み
-RPC_URL=${RPC_URL}
-PRIVATE_KEY=${PRIVATE_KEY}
-
-# デプロイコマンドの実行
-forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/MissionChain.sol:MissionChain
