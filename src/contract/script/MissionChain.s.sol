e00c481e232023f94a186f734e763a4a099c3942
susumu tomita <oyster880@gmail.com>
2024-06-01 08:11:51 +0900

    update deploy script

--- src/contract/script/MissionChain.s.sol
+++ src/contract/script/MissionChain.s.sol
@@ -2,11 +2,19 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
+import {MissionChain} from "../src/MissionChain.sol";
 
 contract MissionChainScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+        // MissionChainコントラクトのデプロイメント
+        MissionChain missionChain = new MissionChain();
+
+        console.log("MissionChain deployed at:", address(missionChain));
+
+        vm.stopBroadcast();
     }
 }
